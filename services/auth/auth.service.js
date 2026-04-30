be7b313cc38fbaea34db58c54eb3e602f00ab0fb
sarthakcana <sarthak.canatech@gmail.com>
2026-04-30 15:29:51 +0530

    fixed sendEmail issue

--- services/auth/auth.service.js
+++ services/auth/auth.service.js
@@ -159,7 +159,7 @@ const registerUser = async (data) => {
              VALUES ($1, $2, $3, $4, $5, $6, $7)`,
             [userId, first_name, last_name, department_id || null, user.user_id, aadhar_number || null, employee_id || null]
         );
-        
+
         await sendWelcomeCredentials({
             email,
             password,
@@ -175,133 +175,6 @@ const registerUser = async (data) => {
         client.release();
     }
 };
-
-
-
-// exports.addEmployee = async (data) => {
-//     const client = await pool.connect();
-//     try {
-//         await client.query("BEGIN");
-
-//         // Check permission (Department Head only)
-//         const check = await client.query(`
-//             SELECT 1 FROM users u
-//             JOIN roles r ON u.role_id = r.role_id
-//             JOIN role_permissions rp ON r.role_id = rp.role_id
-//             JOIN permissions p ON p.permission_id = rp.permission_id
-//             WHERE u.user_id = $1 
-//             AND r.name = 'dept_head'
-//             AND p.name = 'add_employee'
-//         `, [data.user.user_id]);
-
-//         if (check.rowCount === 0) {
-//             throw { status: 403, message: "Only Department Head can add employee" };
-//         }
-
-//         //  Insert into users table
-//         const userRes = await client.query(`
-//             INSERT INTO users (email, phone, password, zp_id, caste_id, role_id)
-//             VALUES ($1, $2, $3, $4, $5, 5)  
-//             RETURNING user_id
-//         `, [
-//             data.email,
-//             data.phone,
-//             data.password,
-//             data.zp_id,
-//             data.caste_id
-//         ]);
-
-//         const newUserId = userRes.rows[0].user_id;
-
-//         // Insert into user_profile
-//         await client.query(`
-//             INSERT INTO user_profile 
-//             (user_id, first_name, last_name, zp_id, department_id, joining_date, created_by)
-//             VALUES ($1, $2, $3, $4, $5, $6, $7)
-//         `, [
-//             newUserId,
-//             data.first_name,
-//             data.last_name,
-//             data.zp_id,
-//             data.department_id,
-//             data.joining_date,
-//             data.user.user_id
-//         ]);
-
-//         await client.query("COMMIT");
-
-//         return { message: "Employee added successfully" };
-
-//     } catch (err) {
-//         await client.query("ROLLBACK");
-//         throw err;
-//     } finally {
-//         client.release();
-//     }
-// };
-
-// exports.addEmployee = async (data) => {
-//     const client = await pool.connect();
-
-//     try {
-//         await client.query("BEGIN");
-
-//         // Check permission (Department Head only)
-//         const check = await client.query(`
-//             SELECT 1 FROM users u
-//             JOIN roles r ON u.role_id = r.role_id
-//             JOIN role_permissions rp ON r.role_id = rp.role_id
-//             JOIN permissions p ON p.permission_id = rp.permission_id
-//             WHERE u.user_id = $1 
-//             AND r.name = 'Department Head'
-//             AND p.name = 'Add Employee'
-//         `, [data.user.user_id]);
-
-//         if (check.rowCount === 0) {
-//             throw { status: 403, message: "Only Department Head can add employee" };
-//         }
-
-//         //  Insert into users table
-//         const userRes = await client.query(`
-//             INSERT INTO users (email, phone, password, zp_id, caste_id, role_id)
-//             VALUES ($1, $2, $3, $4, $5, 5)  
-//             RETURNING user_id
-//         `, [
-//             data.email,
-//             data.phone,
-//             data.password,
-//             data.zp_id,
-//             data.caste_id
-//         ]);
-
-//         const newUserId = userRes.rows[0].user_id;
-
-//         // Insert into user_profile
-//         await client.query(`
-//             INSERT INTO user_profile 
-//             (user_id, first_name, last_name, zp_id, department_id, joining_date, created_by)
-//             VALUES ($1, $2, $3, $4, $5, $6, $7)
-//         `, [
-//             newUserId,
-//             data.first_name,
-//             data.last_name,
-//             data.zp_id,
-//             data.department_id,
-//             data.joining_date,
-//             data.user.user_id
-//         ]);
-
-//         await client.query("COMMIT");
-
-//         return { message: "Employee added successfully" };
-
-//     } catch (err) {
-//         await client.query("ROLLBACK");
-//         throw err;
-//     } finally {
-//         client.release();
-//     }
-// };
 
 exports.loginUser = async ({ email, password, zp_name }) => {
     if (!email || !password) {
